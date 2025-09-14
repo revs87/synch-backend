43ac42426849bd94beef397a4fb69f67d542a623
Rui Vieira <revs87@msn.com>
2025-09-14 18:26:40 +0100

    Added modules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,14 @@
 pluginManagement {
-	repositories {
-		maven { url = uri("https://repo.spring.io/snapshot") }
-		gradlePluginPortal()
-	}
+    repositories {
+        maven { url = uri("https://repo.spring.io/milestone") }
+        maven { url = uri("https://repo.spring.io/snapshot") }
+        gradlePluginPortal()
+    }
 }
 rootProject.name = "synch"
+
+include("app")
+include("user")
+include("chat")
+include("notification")
+include("common")
---
+++ user/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.jpa)
+    alias(libs.plugins.spring.boot)
+    alias(libs.plugins.spring.dependency.management)
+    alias(libs.plugins.kotlin.spring)
+}
+
+group = "com.rvcoding"
+version = "unspecified"
+
+repositories {
+    mavenCentral()
+    maven { url = uri("https://repo.spring.io/milestone") }
+    maven { url = uri("https://repo.spring.io/snapshot") }
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
