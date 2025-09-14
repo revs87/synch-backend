b27f9f3158698e90a88886d4efa31989a54dc8ee
Rui Vieira <revs87@msn.com>
2025-09-14 16:22:59 +0100

    Base project

---
+++ src/main/kotlin/com/rvcoding/synch/SynchApplication.kt
@@ -0,0 +1,11 @@
+package com.rvcoding.synch
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SynchApplication
+
+fun main(args: Array<String>) {
+	runApplication<SynchApplication>(*args)
+}
---
+++ src/test/kotlin/com/rvcoding/synch/SynchApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.rvcoding.synch
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SynchApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
