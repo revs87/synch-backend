deaa796519243077696b49f0d6e7e886f837a18e
Rui Vieira <revs87@msn.com>
2025-09-15 23:07:47 +0100

    User domain model objects

---
+++ user/src/main/kotlin/com/rvcoding/user/domain/model/AuthenticatedUser.kt
@@ -0,0 +1,7 @@
+package com.rvcoding.user.domain.model
+
+data class AuthenticatedUser(
+    val user: User,
+    val accessToken: String,
+    val refreshToken: String
+)
---
+++ user/src/main/kotlin/com/rvcoding/user/domain/model/User.kt
@@ -0,0 +1,12 @@
+package com.rvcoding.user.domain.model
+
+import java.util.UUID
+
+typealias UserId = UUID
+
+data class User(
+    val id: UserId,
+    val username: String,
+    val email: String,
+    val hasEmailVerified: Boolean
+)
