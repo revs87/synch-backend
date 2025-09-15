6cee5ccaeb8de94031cc4d66c4dfbebd2f7d651b
Rui Vieira <revs87@msn.com>
2025-09-15 23:35:21 +0100

    Created User Database Entity

---
+++ user/src/main/kotlin/com/rvcoding/user/infra/database/entities/UserEntity.kt
@@ -0,0 +1,40 @@
+package com.rvcoding.user.infra.database.entities
+
+import com.rvcoding.user.domain.model.UserId
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Index
+import jakarta.persistence.Table
+import java.time.Instant
+import org.hibernate.annotations.CreationTimestamp
+import org.hibernate.annotations.UpdateTimestamp
+
+@Entity
+@Table(
+    name = "users",
+    schema = "user_service", // same as in Supabase
+    indexes = [
+        Index(name = "idx_users_email", columnList = "email"),
+        Index(name = "idx_users_username", columnList = "username"),
+    ]
+)
+class UserEntity( // data classes are immutable
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    var id: UserId? = null,
+    @Column(nullable = false, unique = true)
+    var email: String,
+    @Column(nullable = false, unique = true)
+    var username: String,
+    @Column(nullable = false, unique = true)
+    var hashedPassword: String,
+    @Column(nullable = false)
+    var hasVerifiedEmail: Boolean,
+    @CreationTimestamp
+    var createdAt: Instant = Instant.now(),
+    @UpdateTimestamp
+    var updatedAt: Instant = Instant.now()
+)
